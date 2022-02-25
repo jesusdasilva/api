ba85c544a37efcfe699185d206af4185c2019779
Hikari-rom <romainfpro@gmail.com>
2022-02-25 15:32:30 +0100

    fix: :bug: WIP : Start to fix the request of findOneByEmail

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -26,11 +26,12 @@ class UserRepository extends ServiceEntityRepository
     public function findOneByEmail(string $email): ?User
     {
         return $this->_em->createQueryBuilder()
-            ->select('User')
-
-            ->where('email = :email')
+            ->select('u')
+            ->from('App\Entity\User', 'u')
+            ->where('u.email = :email')
             ->setParameter('email', $email)
 
             ->getQuery()->getOneOrNullResult();
     }
+
 }
